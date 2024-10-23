d010008ccf2c295c642a8b4d867edd9476721d58
Juan Carlos <juancarlosgmr1460@gmail.com>
2024-10-23 05:29:04 -0300

    continuacao do modal de perfil

--- src/componentes/Modals/PerfilModal/PerfilModal.js
+++ src/componentes/Modals/PerfilModal/PerfilModal.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './PerfilModal.css'
 import fotoPerfil from '../../../imgs/gaxinimEmoji.png';
+import logoPerfil from '../../../imgs/logoPerfil.png';
+import logoConfig from '../../../imgs/configPerfilModal.png';
+import logoSair from '../../../imgs/logoSairModal.png';
 import Texto from '../../Texto';
 
 export default function PerfilModal({ isOpen }) {
@@ -13,6 +16,20 @@ export default function PerfilModal({ isOpen }) {
                     </div>
                     <Texto peso='normal' tamanho='18px' cor='#555'>Guanixim</Texto>
                     <Texto peso='600' tamanho='15px' cor='#555'>exampleEmail@lorinIpsum.com</Texto>
+                </div>
+                <div id='configPerfi'>
+                    <div className='txtConfigPerfil'>
+                        <img src={logoPerfil} height='20px' width='20px' alt='' />
+                        <Texto peso='normal' tamanho='16px' cor='#555'>Perfil</Texto>
+                    </div>
+                    <div className='txtConfigPerfil'>
+                        <img src={logoConfig} height='20px' width='20px' alt='' />
+                        <Texto peso='normal' tamanho='16px' cor='#555'>Configuracao</Texto>
+                    </div>
+                    <div className='txtConfigPerfil'>
+                        <img src={logoSair} height='20px' width='20px' alt='' />
+                        <Texto peso='normal' tamanho='16px' cor='#555'>Sair</Texto>
+                    </div>
                 </div>
 
 
